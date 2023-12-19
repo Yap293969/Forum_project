726fafbf43fdcb7b919a2d16622070a486f7afae
teegaiyu <134262481+teegaiyu@users.noreply.github.com>
2023-12-19 14:19:08 +0800

    navbar update (GY)
    change navbar height and add a side bar for the navbar

--- navbar.php
+++ navbar.php
@@ -14,115 +14,62 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
         crossorigin="anonymous"></script>
 <style>
-  
-
-.h7 {
-    font-size: 0.8rem;
-}
-
-.gedf-wrapper {
-    margin-top: 0.97rem;
-}
-
-       
-        /* Styles for the dropdown */
-        .dropdown {
-            display: inline-block;
-            align-items: center;
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            background-color: #333;
-            min-width: 160px;
-            max-height: 100px;
-            box-shadow: 0 8px 16px rgba(0,0,0,0.2);
-        }
-
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            display: block;
-            text-align: left;
-            text-decoration: none;
-        }
-
-        .dropdown-content a:hover {
-            background-color: #fff;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-            background-color: lightblue;
-        }
-
-       
-
-
-
-@media (min-width: 992px) {
-    .gedf-main {
-        padding-left: 4rem;
-        padding-right: 4rem;
-    }
-    .gedf-card {
-        margin-bottom: 2.77rem;
-    }
-}
+  .navbar bg-body-tertiary{
+    background-color:blue;
+  }
 </style>
-<nav class="navbar navbar blue" style="background-color: blue;">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="margin-bottom: 50px; background-color:blue; align-items: center;" >
-        <div class="container-fluid" style="margin-left: 200px;">
-          <a class="navbar-brand" href="#" style="color: lightblue;">Bluedit</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent" style= margin:10px; >
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0"  style="text-align: center;">
-              <li class="nav-item" style="margin: 5px;">
-                <a class="nav-link active" aria-current="page" href="#" style="color: #fff;">Home</a>
-              </li>
-              <li class="nav-item" style="margin: 5px;">
-                <a class="nav-link" href="#" style="color: #fff;">Popular</a>
-              </li>
-              <li class="nav-item" style="margin: 5px;">
-                <a class="nav-link" href="#" style="color: #fff;">Blog</a>
-              </li>
-              <li class="nav-item" style="margin: 5px;">
-                <a class="nav-link" href="#" style="color: #fff;">Advertise</a>
-              </li>
-              <li class="dropdown" style="padding: 8px; margin: 5px;">
-                <a href="#" class="dropbtn" style="color: #fff;">About</a>
-                <div class="dropdown-content">
-                    <a href="#">About Careers</a>
-                    <a href="#">About Forum</a>
-                </div>
-            </li>
-            <li class="nav-item" style="margin: 5px;">
-                <a class="nav-link" href="#" style="color: #fff;">Careers</a>
-              </li>
-                 
-            <div id="search-container" style="margin-left: 250px;">
+<nav class="navbar navbar-dark bg-primary" style="margin-bottom:10px;">
+  
+    <a class="navbar-brand" href="#">Bluedit</a>
+    <div id="search-container">
                 
-                    <input type="text" id="search-input" placeholder="Enter your search" style="width: 300px; height: 50px; border-radius: 10px; text-align: center;">
-                    <button id="search-button" onclick="performSearch()" style="background-color: blue; border-style: none;"><i class="fa fa-search"></i></button>
-                
-            </div>
+                <input type="text" id="search-input" placeholder="Enter your search" style="width: 300px; height: 40px; border-radius: 10px; text-align: center; margin-left:250px;">
+                <button id="search-button" onclick="performSearch()" style="background-color: transparent; border-style: none;"><i class="fa fa-search"></i></button>
             
-                <script>
-                    function performSearch() {
-                        // Get the search query from the input field
-                        var searchQuery = document.getElementById('search-input').value;
-            
-                        // Redirect to Google with the search query
-                        window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(searchQuery);
-                    }
-                </script>
-            </ul>
-           
-                
-          </div>
         </div>
-      </nav>
-    </nav>
+        
+            <script>
+                function performSearch() {
+                    // Get the search query from the input field
+                    var searchQuery = document.getElementById('search-input').value;
+        
+                    // Redirect to Google with the search query
+                    window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(searchQuery);
+                }
+            </script>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+       
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Bluedit</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+       
+      </div>
+    </div>
+  </div>
+</nav>
