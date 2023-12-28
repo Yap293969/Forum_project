60a2ea6f11c6ffc1e58dd223dc6fbada798d0977
teegaiyu <134262481+teegaiyu@users.noreply.github.com>
2023-12-28 10:04:49 +0800

    create community (GY)
    database connect successfully can be insert to phpmyadmin

---
+++ create_community.php
@@ -0,0 +1,209 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!------ Include the above in your HEAD tag ---------->
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+        crossorigin="anonymous">
+        
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+        crossorigin="anonymous">
+        
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v18.0" nonce="07DquqDk"></script>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v18.0" nonce="IXaqmzjl"></script>
+        <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v18.0" nonce="9gOKuddq"></script>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v18.0" nonce="A6Wplw0u"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<!------ Include the above in your HEAD tag ---------->
+<style>
+    body{
+        background-color:lightblue;
+    }
+    h1{
+        text-align:center;
+    }
+    .main{
+ 	padding: 40px 0;
+}
+.main input,
+.main input::-webkit-input-placeholder {
+    font-size: 10px;
+    padding-top: 3px;
+}
+.main-center{
+ 	margin-top: 30px;
+ 	margin: 0 auto;
+ 	max-width: 400px;
+    padding: 10px 40px;
+	background:#009edf;
+	    color: #FFF;
+    text-shadow: none;
+	-webkit-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.31);
+-moz-box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.31);
+box-shadow: 0px 3px 5px 0px rgba(0,0,0,0.31);
+
+}
+span.input-group-addon i {
+    color: #009edf;
+    font-size: 17px;
+}
+#dropdown {
+        padding: 2px;
+        font-size: 10px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        width: 320px;
+        height:40px;
+        color:black;
+        margin-bottom:10px;
+    }
+
+    /* Styling for dropdown options when hovered */
+    #dropdown option:hover {
+        background-color: #f5f5f5;
+    }
+
+    /* Styling for dropdown options when selected */
+    #dropdown option:checked {
+        background-color: #e0e0e0;
+    }
+
+      /* Basic styling for radio buttons */
+      .radio-container {
+        margin-bottom: 15px;
+    }
+
+    .radio-container label {
+        margin-right: 15px;
+    }
+
+    .radio-container input[type="radio"] {
+        margin-right: 5px;
+    }
+
+</style>
+<?php
+
+    if(isset($_POST['submit'])){
+ 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="test_database_management";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }else{
+        echo "";
+        }
+
+ $sql = "INSERT INTO community_create (name_comm, url_link, descrip,communitytype,public_private)
+                VALUES ('".$_POST['name']."', '".$_POST['link']."', '".$_POST['description']."',
+                '".$_POST['type_commnu']."','".$_POST['type']."')";
+            if ($conn->query($sql) === TRUE) {
+            echo '<div style="font-size:15px; text-align:center;" class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Congratulations!</strong> Your community has been created successfully!
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>';
+            } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+ $conn->close();
+}
+
+?>
+<body style="background-color:lightblue;">
+<h1 style="margin-top:30px;">Create Community</h1>
+	<div class="container" style="margin-top:30px;">
+			<div class="main">
+				<div class="main-center">
+					<form class="" method="post">
+						
+						<div class="form-group">
+							<label for="name">Community Name:</label>
+								<div class="input-group">
+				<input type="text" class="form-control" name="name" id="name"  placeholder="Enter your Community Name:"/>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="URL">URL:</label>
+								<div class="input-group">
+									<input type="link" class="form-control" name="link" placeholder="Enter your Community Link"/>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="type">Community Description:</label>
+								<div class="input-group">
+									<input type="text" class="form-control" name="description" placeholder="Enter your Community Description"/>
+								</div>
+						</div>
+                        <label for="type">Choose your type community<label>
+                        <select id="dropdown" name="type_commnu">
+                            <option value="Foods">Foods Community</option>
+                            <option value="Travel">Travel Community</option>
+                            <option value="Movie">Movie Community</option>
+                            <option value="Games">Games Community</option>
+                            <option value="Other">Other</option>
+                        </select>   
+
+                        <div class="radio-container">
+                            <label>
+                                <input type="radio" name="type" value="public">
+                                Public:
+                            </label>
+                            <label>
+                                <input type="radio" name="type" value="private">
+                                Private:
+                            </label>
+                            
+                        </div>
+
+				<button  name="submit" style="cursor:pointer; width: 320px; height: 30px; border-radius:20px; color:black;" type="submit">SUBMIT</button>
+						
+					</form>
+				</div><!--main-center"-->
+			</div><!--main-->
+		</div><!--container-->
+        <?php
+            include 'footer.php';
+        ?>
+</body>
+        
