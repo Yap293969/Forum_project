0899ada15737acc6df849567404743123626da0f
teegaiyu <134262481+teegaiyu@users.noreply.github.com>
2024-01-06 16:06:44 +0800

    navbar (GY)
    update navbar

--- navbar.php
+++ navbar.php
@@ -1,76 +1,130 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<!------ Include the above in your HEAD tag ---------->
-
-<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous">
-        
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Professional Navigation Drawer</title>
+<link rel="stylesheet" href="styles.css">
+</head>
 <style>
-  .navbar bg-body-tertiary{
-    background-color:blue;
+  body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+.menu-button {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  background: none;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  z-index: 3;
+}
+
+.bar {
+  display: block;
+  width: 25px;
+  height: 3px;
+  margin: 5px auto;
+  background-color: #fff;
+
+  transition: 0.3s;
+}
+
+.drawer {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  background-color: #fff;
+  overflow-x: hidden;
+  transition: 0.5s;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+}
+
+.drawer ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 50px 0 0 0;
+}
+
+.drawer a {
+  display: block;
+  padding: 15px 20px;
+  color: #333;
+  text-decoration: none;
+  transition: 0.3s;
+}
+
+.drawer a:hover {
+  background-color: #f9f9f9;
+}
+
+.close-button {
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  padding: 10px 20px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.close-button:hover {
+  background-color: #555;
+}
+.navbar {
+    background-color: #333;
+    color: #fff;
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
   }
+  .navbar h1 {
+    margin: 20px;
+    margin-left:600px;
+  }
+
 </style>
-<nav class="navbar navbar-dark bg-primary" style="margin-bottom:10px;">
-  
-    <a class="navbar-brand" href="#">Bluedit</a>
-    
-    <div id="search-container">
-                
-                <input type="text" id="search-input" placeholder="Enter your search" style="width: 350px; height: 40px; border-radius: 10px; text-align: center; margin-left:900px;">
-                <button id="search-button" onclick="performSearch()" style="background-color: transparent; border-style: none;"><i class="fa fa-search"></i></button>
-            
-        </div>
-        
-            <script>
-                function performSearch() {
-                    // Get the search query from the input field
-                    var searchQuery = document.getElementById('search-input').value;
-        
-                    // Redirect to Google with the search query
-                    window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(searchQuery);
-                }
-            </script>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-       
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Bluedit</h5>
-        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
-       
-      </div>
-    </div>
-  </div>
-</nav>
+<body>
+
+<div class="navbar" style="background-color: lightblue;">
+  <h1>My Website</h1>
+  <button id="openDrawerButton" class="menu-button">
+  <span class="bar"></span>
+  <span class="bar"></span>
+  <span class="bar"></span>
+</button>
+</div>
+
+
+<div id="myDrawer" class="drawer">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Services</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+  <button id="closeDrawerButton" class="close-button">Close</button>
+</div>
+
+<script>
+  document.getElementById("openDrawerButton").addEventListener("click", function() {
+  document.getElementById("myDrawer").style.width = "250px";
+});
+
+document.getElementById("closeDrawerButton").addEventListener("click", function() {
+  document.getElementById("myDrawer").style.width = "0";
+});
+
+</script>
+
+</body>
+</html>
