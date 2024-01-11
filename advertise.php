d8852d205f43e3f367ab21bb851a26048127ff0a
teegaiyu <134262481+teegaiyu@users.noreply.github.com>
2024-01-11 13:58:30 +0800

    final advertise page (GY)
    update images

--- advertise.php
+++ advertise.php
@@ -10,7 +10,7 @@ body {
     height: 100%;
     min-height: 1000px;
     background-size: cover;
-    background-repeat: no-repeat;
+    background-position:fixed;
     font-family: 'Roboto';
     }
 
@@ -91,7 +91,7 @@ body {
                   <div class="item-date">05/02/2021</div>
                     <div class="item-category"><a href="#">Top 1</a></div>
                     <div class="item-title clearfix "><h1 class="text-light pt-3 ">Drama</h1></div>
-        <div class="icon my-4"><img src="xingwennwang.jpg"></div>
+        <div class="icon my-4"><img src="the queen of news.jpg"></div>
                   <div class="item-description text-white">"The Queen of News" (English: the QUEEN of news) is a fashionable urban business war drama jointly produced by TV Broadcasting Co., Ltd. and Youku Information Technology (Beijing) Co., Ltd., with the news department and office politics as the axis, starring Charmaine Sheh, Ma Guoming, Li Shi Hua, Gao Haining, Wang Minyi, He Yiting[7], Tang Chi Kin, Tam Junyan, Gong Ci'en[2], Wu Yekun, Che Wanwan, He Guangpei and Lam Zhengfeng star[3], while other actors include Chen Shancong and Gao Junxian[8], producer Zhong Shujia, Guan Wenshen[7]. [9] This drama is also Charmaine Sheh's second collaboration with Ma Guoming after "The Club of Casino Heroes".
                   </div>
                   </div>
@@ -105,7 +105,7 @@ body {
                   <div class="item-date">05/02/2021</div>
                     <div class="item-category"><a href="#">Top 2</a></div>
                     <div class="item-title clearfix "><h1 class="text-light pt-3 ">Movie</h1></div>
-        <div class="icon my-4"><img src="shituxingzhe.jpg"></div>
+        <div class="icon my-4"><img src="使徒行者3.jpg"></div>
                   <div class="item-description text-white">"Walker 3" is produced by Shanghai Tencent Penguin Film and Television Culture Communication Co., Ltd. and Television Broadcasting Co., Ltd., directed by Su Wanchong, starring Raymond Lam, Miu Qiaowei, Ma Guoming, Yuan Weihao, Huang Zhiwen, Huang Cuiru, Cai Sibei, Zhang Zhenlang, Hui Siu-hsiung, Hong Yongcheng, Lien Shi A police drama co-starring Ya Ya and Lin Xuanyu, with special appearances by Zeng Shunxi and Zheng Zeshi[1].The play tells the story of undercover Xue Jiaqiang and Zhuo Kai, a former undercover liaison who was released from prison, secretly cooperate with the police to fight crime and protect justice [1] .
                     The drama premiered on Tencent Video in mainland China on October 12, 2020 [1] and was broadcast on Jade Channel in Hong Kong, China on November 9, 2020 [2] .</div>
                   </div>
@@ -117,7 +117,7 @@ body {
                   <div class="item-date">05/02/2021</div>
                     <div class="item-category"><a href="#">Top 3</a></div>
                     <div class="item-title clearfix "><h1 class="text-light pt-3 ">Movie</h1></div>
-        <div class="icon my-4"><img src="yichujifa.jpg"></div>
+        <div class="icon my-4"><img src="一触即发.jpg"></div>
                   <div class="item-description text-white">"Imminent Crisis" is a military suspense TV series produced by Beijing Guangcai Century Culture and Arts Co., Ltd., directed by Gan Lu, written by Zhang Yong, starring Zhong Hanliang, Yang Rui, Li Yue, Xu Cuicui, Li Liqun, Ding Ziling, Li Ying and others. The play tells the story of a family tragedy in Shanghai in the 1930s that separated twin brothers A Chu and A Ci in their infancy. The two brothers went from hostility, misunderstanding and mutual suspicion to sharing the same hatred and jointly resisting the enemy. In the end, the conspiracy planned by the Japanese invaders for many years was revealed to the world. Ah Ci sacrificed his life for the country. Ah Chu shouldered the hatred of the country and family, and continued to lurk in the name of Ah Ci. Come down... >>></div>
                   </div>
                 </div>
