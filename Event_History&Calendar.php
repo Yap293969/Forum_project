97ae8d12e661e30c75da3b72baf5ec47eff1240f
WongLokBin(Yaku00) <wlb964@gmail.com>
2024-01-28 16:24:48 +0800

    Update Event_History&Calendar.php

--- Event_History&Calendar.php
+++ Event_History&Calendar.php
@@ -231,7 +231,7 @@
 
     <nav aria-label="Page navigation example" role="navigation">
         <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li> 
             <li class="page-item"><a class="page-link" href="#">1</a></li>
             <li class="page-item"><a class="page-link" href="#">2</a></li>
             <li class="page-item"><a class="page-link" href="#">3</a></li>
