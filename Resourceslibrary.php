ec9b320511446aefebf8b2cd3b0475ee597b255f
WongLokBin(Yaku00) <wlb964@gmail.com>
2024-01-28 16:24:42 +0800

    Update Resourceslibrary.php

--- Resourceslibrary.php
+++ Resourceslibrary.php
@@ -1,95 +1,105 @@
 <!DOCTYPE html>
 <html>
+    <title>
+
+    </title>
     <head>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <link rel="stylesheet" href="CSS/resourceslibrary.css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <!------ Include the above in your HEAD tag ---------->
+        <link rel="stylesheet" href="CSS/resourceslibrary.css">
     </head>
-    <body>
-        
 
+    <body>
     <?php
-    include '';
+    include 'navbar.php';
     ?>
 
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <form class="search-form">
-                    <div class="input-group">
-                        <input id="input-1" class="form-control" type="text" aria-describedby="search-btn">
-                        <label for="input-1" class="sr-only">Search</label>
-                        <div class="input-group-append">
-                            <button id="search-btn" class="btn btn-primary">Search</button>
+            <div id="sp-component" class="col-sm-12 col-md-12">
+                <div class="sp-column ">
+                </div>
+                <div class="row">
+                <div class="col-sm-4">
+                    <div class="documentation-index">
+                        <a href="/documentation/sp-page-builder/sp-page-builder-2-x">
+                        <img src="//www.joomshaper.com/images/2017/01/27/page-builder.png" alt="SP Page Builder">
+                        </a>
+                        <h2>SP Page Builder</h2>
+                        <div class="documentation-shortdesc">
+                            The complete knowledge base and best crafted guide to SP Page Builder you will ever need. 
                         </div>
+                        <a class="btn btn-doc-outline" href="/documentation/sp-page-builder/sp-page-builder-2-x">View Documentation</a>
                     </div>
-                </form>
-                <ul class="topics-table">
-                    <li class="topics-header">
-                    <ul class="header-titles">
-                        <li>Forum</li>
-                        <li>Topics</li>
-                        <li>Posts</li>
-                        <li>Freshness</li>
-                    </ul>
-                    </li>
-                    <li class="topics-body">
-                        <ul class="topic-item-1">
-                            <li>
-                                <a class="badge badge-primary" href="">Around the Boards</a>
-                            </li>
-                            <li>4</li>
-                            <li>5</li>
-                            <li>
-                                
-                                <a class="badge badge-info" href="http://skatingbuzz.test/forums/topic/random-4/" title="Random 4">1 week ago</a>
-                                <p class="bbp-topic-meta">
-                                    <span class="bbp-topic-freshness-author">
-                                        <a 
-                                        href="http://skatingbuzz.test/forums/user/admin/" 
-                                        title="View admin's profile" 
-                                        class="bbp-author-name badge badge-info" 
-                                        rel="nofollow">
-                                            admin
-                                        </a>
-                                    </span>
-                                </p>
-                            </li>
-                        </ul>
-                        <ul class="topic-item-2">
-                            <li>
-                                <a class="badge badge-primary" href="">Skating Events</a>
-                            </li>
-                            <li>2</li>
-                            <li>4</li>
-                            <li>
-                                
-                                <a class="badge badge-info" href="http://skatingbuzz.test/forums/topic/random-4/" title="Random 4">1 day ago</a>
-                                <p class="bbp-topic-meta">
-                                    <span class="bbp-topic-freshness-author">
-                                        <a 
-                                        href="http://skatingbuzz.test/forums/user/admin/" 
-                                        title="View admin's profile" 
-                                        class="bbp-author-name badge badge-info" 
-                                        rel="nofollow">
-                                            leslie
-                                        </a>
-                                    </span>
-                                </p>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
+                </div>
+                <div class="col-sm-4">
+                    <div class="documentation-index">
+                        <a href="/documentation/joomla-templates/rhino">
+                        <img src="//www.joomshaper.com/images/2017/01/27/template.png" alt="Joomla Templates">
+                        </a>
+                        <h2>Joomla Templates</h2>
+                        <div class="documentation-shortdesc">
+                            Explore our extensive resources to get an in-depth understanding on our Joomla templates. 
+                        </div>
+                        <a class="btn btn-doc-outline" href="/documentation/joomla-templates/rhino">View Documentation</a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="documentation-index">
+                        <a href="/documentation/joomla-extensions/sp-easyimagegallery">
+                        <img src="//www.joomshaper.com/images/2017/01/27/extensions.png" alt="Joomla Extensions">
+                        </a>
+                        <h2>Joomla Extensions</h2>
+                        <div class="documentation-shortdesc">
+                            Learn ins and outs of our powerful, innovative and world wide used extensions for Joomla.
+                        </div>
+                        <a class="btn btn-doc-outline" href="/documentation/joomla-extensions/sp-easyimagegallery">View Documentation</a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="documentation-index">
+                        <a href="/documentation/helix-framework/helix3">
+                        <img src="//www.joomshaper.com/images/2017/01/27/helix.png" alt="Helix Framework">
+                        </a>
+                        <h2>Helix Framework</h2>
+                        <div class="documentation-shortdesc">
+                            Learn everything about the Helix Framework. What’s Helix, how to use it and much more. 
+                        </div>
+                        <a class="btn btn-doc-outline" href="/documentation/helix-framework/helix3">View Documentation</a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="documentation-index">
+                        <a href="https://www.joomshaper.com/faqs">
+                            <img src="//www.joomshaper.com/images/2017/01/27/faq.png" alt="Faqs">
+                        </a>
+                        <h2>Faqs</h2>
+                        <div class="documentation-shortdesc">
+                            A set of ready answers to the most common questions asked by the users and members. 
+                        </div>
+                        <a class="btn btn-doc-outline" href="https://www.joomshaper.com/faqs">View Faqs</a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="documentation-index">
+                        <a href="https://www.joomshaper.com/forums">
+                        <img src="//www.joomshaper.com/images/2017/01/27/forums.png" alt="Forums">
+                        </a>
+                        <h2>Forums</h2>
+                        <div class="documentation-shortdesc">
+                            Share your experience and get solutions to any issue you may face with our products. 
+                        </div>
+                        <a class="btn btn-doc-outline" href="https://www.joomshaper.com/forums">Access Forums</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 
     <?php
-    include '';
+        include 'footer.php';
     ?>
-
     </body>
-
 </html>
